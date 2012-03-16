6d2724c8e197051f0f474c20cad660d8ef6eadf1
Zach Moazeni <zach@collectiveidea.com>
2012-03-15 20:30:32 -0400

    Experimenting with richer data types for code readability

--- Parser.hs
+++ Parser.hs
@@ -11,27 +11,31 @@ import Data.JSON2.Query
 import qualified Data.Map as M
 import Data.Maybe
 
-parseInvertedIndex :: String -> M.Map String [Integer]
+type Gram       = String
+type JsonId     = Integer
+type JsonObject = M.Map String Json
+
+parseInvertedIndex :: String -> M.Map Gram [JsonId]
 parseInvertedIndex = invertIdsAndGrams . idsAndGrams . idsAndText . toJson
 
-invertIdsAndGrams :: M.Map Integer [String] -> M.Map String [Integer]
+invertIdsAndGrams :: M.Map JsonId [Gram] -> M.Map Gram [JsonId]
 invertIdsAndGrams = foldr mapGramsToIds M.empty . M.toList
   where 
     mapGramsToIds (id, []) gramIdMap = gramIdMap
     mapGramsToIds (id, (gram:grams)) gramIdMap = M.insertWith uniqInsert gram [id] $ mapGramsToIds (id, grams) gramIdMap
     uniqInsert (newValue:newValues) oldValues = if newValue `elem` oldValues then oldValues else newValue:oldValues
 
-idsAndGrams :: M.Map Integer String -> M.Map Integer [String]
+idsAndGrams :: M.Map JsonId String -> M.Map JsonId [Gram]
 idsAndGrams = M.map toGrams
   where toGrams string = words $ map toLower string
 
-idsAndText :: [M.Map String Json] -> M.Map Integer String
+idsAndText :: [JsonObject] -> M.Map JsonId String
 idsAndText jsons = foldr mapIdToText M.empty jsons
   where mapIdToText json map       = M.insert (getId $ M.lookup "id" json) (getText $ M.lookup "text" json) map
         getId (Just (JNumber x))   = truncate x
         getText (Just (JString x)) = x
 
-toJson :: String -> [M.Map String Json]
+toJson :: String -> [JsonObject]
 toJson = map processJson . lines
   where processJson rawJson = case parseJson rawJson of
                                 Right (JObject x)  -> x
