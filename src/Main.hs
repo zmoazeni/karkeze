824c8647b7253e8153f4d892a9930a1689db1b45
Zach Moazeni <zach@collectiveidea.com>
2012-03-28 01:44:19 -0400

    Trying unsuccessfully to retrieve the request body

--- src/Main.hs
+++ src/Main.hs
@@ -11,23 +11,27 @@ main = do
   args <- getArgs
   let dbPath = databasePath args
   withDB dbPath $ \db ->
-    case args of
-      ("read":rawGram:_)     -> readGram db (Gram rawGram)
-      ("load":_)             -> loadIndex db "input.json"
-      ("grams":_)            -> printGrams db
-      ("rawGrams":_)         -> grams db >>= print
-      ("rawKeys":_)          -> keys db >>= print
-      ("print":_)            -> parseAndPrint "input.json"
-      ("web":port:_)         -> run port db
-      ("example":_)          -> example db
-      ("concurrencytest1":_) -> badConcurrency db
-      ("concurrencytest2":_) -> destroy dbPath [] >> separateKeys db
-      _                      -> putStrLn "[load|print|grams|read <gram>|web <port>|example|concurrencytest1|concurrencytest2]"
+    withDB stageDBPath $ \stageDB ->
+      case args of
+        ("read":rawGram:_)     -> readGram db (Gram rawGram)
+        ("load":_)             -> loadIndex db "input.json"
+        ("grams":_)            -> printGrams db
+        ("rawGrams":_)         -> grams db >>= print
+        ("rawKeys":_)          -> keys db >>= print
+        ("print":_)            -> parseAndPrint "input.json"
+        ("web":port:_)         -> run port (db, stageDB)
+        ("example":_)          -> example db
+        ("concurrencytest1":_) -> badConcurrency db
+        ("concurrencytest2":_) -> destroy dbPath [] >> separateKeys db
+        _                      -> putStrLn "[load|print|grams|read <gram>|web <port>|example|concurrencytest1|concurrencytest2]"
 
 databasePath :: [String] -> FilePath
 databasePath ("concurrencytest1":_) = "./db/db-concurrencytest"
 databasePath ("concurrencytest2":_) = "./db/db-concurrencytest"
 databasePath _                     = "./db/db-leveldbtest"
+
+stageDBPath :: FilePath
+stageDBPath = "./db/db-stage"
 
 -- To get a current time sha2:
 -- import Data.Time.Clock.POSIX and Codec.Digest.SHA Data.Serialize
--- src/Web.hs
+++ src/Web.hs
@@ -1,25 +1,45 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports #-}
+
 module Web where
 
 import Web.Scotty
 import Data.JSON2
 import Storage
 import Parser
-import Control.Monad.Trans
-import Data.Text.Lazy (pack)
+import Data.Text.Lazy (Text, pack)
 import Database.LevelDB (DB)
+import Control.Monad.IO.Class
+import Network.HTTP.Types
+import Network.Wai
+import "mtl" Control.Monad.Reader
+import Data.Conduit.Lazy (lazyConsume)
+import qualified Data.ByteString.Lazy.Char8 as BL
+import Control.Applicative
+import Control.Monad.Trans.Resource (runResourceT)
 
-run :: String -> DB -> IO ()
-run port db = scotty (read port) $ do
-  get "/grams" $ do
+type Param = (Text, Text)
+
+run :: String -> (DB, DB) -> IO ()
+run port (db, stageDB) = scotty (read port) $ do
+  get "/" $ do
     grams' <- fetchGrams
     text . pack . toString . toJson $ grams'
     header "Content-Type" "application/json"
 
-  get "/hello" $ do
-    text "[{\"foo\":1}]"
-    header "Content-Type" "application/json"
+  post "/" $ do
+    r <- getBody
+    liftIO $ print r
+    v <- param "foo"
+    liftIO $ putStrLn v
+    status status201
 
   where
     fetchGrams :: ActionM [Gram]
     fetchGrams = liftIO (grams db)
+
+    getBody :: ActionM BL.ByteString
+    getBody =
+      do r <- request
+         body <- liftIO . runResourceT $ BL.fromChunks <$> (lazyConsume . requestBody) r
+         return body
