91765bdfb04ad8792158298a5ae2a8fc509eccb8
Zach Moazeni <zach@collectiveidea.com>
2012-04-01 13:21:36 -0400

    STM is finally starting to click

--- experiment/STMTest.hs
+++ experiment/STMTest.hs
@@ -12,33 +12,49 @@ import Control.Concurrent.STM.Stats
 
 main :: IO ()
 main = do
-  let n = 10
+  let n = 100
   group <- new
 
-  -- v <- newIORef ("a", 1)
-  -- -- let run = randomSleep >> modifyIORef v update
-  -- let run = do randomSleep
-               -- (s, _) <- readIORef v
+  v <- newIORef ("0", fromIntegral 0)
+  -- let run = randomSleep >> modifyIORef v update
+  let run = do yield
+               val <- readIORef v
                -- randomSleep
-               -- writeIORef v (s ++ "a", (length s) + 1)
-  -- n `replicateM_` (G.forkIO group run)
+               yield
+               writeIORef v (update val)
+  n `replicateM_` (G.forkIO group run)
   -- modifyIORef v update
-  -- wait group
-  -- (_, i) <- readIORef v
-  -- putStrLn . ("IORef " ++) $ show i
+  wait group
+  (i, _) <- readIORef v
+  putStrLn . ("IORef " ++) $ show i
 
-  v <- trackSTM $ newTVar ("a", 1)
+  -- v <- trackSTM $ newTVar ("0", fromIntegral 0)
+  -- -- let run = randomSleep >> atomically (modifyTVar v update)
+  -- let run = do randomSleep
+               -- trackNamedSTM "runner" $
+                 -- do unsafeIOToSTM yield
+                    -- val <- readTVar v
+                    -- unsafeIOToSTM yield
+                    -- writeTVar v (update val)
+
+  -- n `replicateM_` (G.forkIO group run)
+  -- -- atomically $ modifyTVar v update
+  -- wait group
+  -- (i, _) <- trackNamedSTM "reader" $ readTVar v
+  -- putStrLn . ("TVar " ++) $ show i
+  -- dumpSTMStats
+
+  v <- trackSTM $ newTVar ("0", fromIntegral 0)
   -- let run = randomSleep >> atomically (modifyTVar v update)
   let run = do randomSleep
-               trackNamedSTM "runner" $
-                 do (s, _) <- readTVar v
-                    unsafeIOToSTM randomSleep
-                    writeTVar v (s ++ "a", (length s) + 1)
+               val <- trackNamedSTM "getter" $ readTVar v
+               yield
+               trackNamedSTM "setter" $ writeTVar v (update val)
 
   n `replicateM_` (G.forkIO group run)
   -- atomically $ modifyTVar v update
   wait group
-  (_, i) <- trackNamedSTM "reader" $ readTVar v
+  (i, _) <- trackNamedSTM "reader" $ readTVar v
   putStrLn . ("TVar " ++) $ show i
   dumpSTMStats
 
@@ -56,14 +72,33 @@ main = do
   -- (_, i) <- (atomically $ readTVar v)
   -- putStrLn . ("TVar " ++) $ show i
 
-  -- v <- atomically $ newTMVar ("a", 1)
-  -- let run = randomSleep >> atomically (modifyTMVar v update)
-      -- modifyTMVar var f = do { val <- takeTMVar var; putTMVar var (f val)}
-  -- n `replicateM_` (G.forkIO group run)
+  v <- trackSTM $ newTMVar ("0", fromIntegral 0)
+  let run = do randomSleep
+               val <- trackNamedSTM "getter" $ takeTMVar v
+               yield
+               trackNamedSTM "setter" $ putTMVar v (update val)
+
+  n `replicateM_` (G.forkIO group run)
   -- atomically $ modifyTMVar v update
+  wait group
+  (i, _) <- trackNamedSTM "reader" $ readTMVar v
+  putStrLn . ("TMVar " ++) $ show i
+  dumpSTMStats
+
+  -- v <- atomically $ newTMVar ("0", fromIntegral 0)
+  -- let run = do randomSleep
+               -- val <- atomically $ takeTMVar v
+               -- yield
+               -- atomically $ putTMVar v (update val)
+
+  -- n `replicateM_` (G.forkIO group run)
+  -- -- atomically $ modifyTMVar v update
   -- wait group
-  -- (_, i) <- (atomically $ readTMVar v)
+  -- (i, _) <- (atomically $ readTMVar v)
   -- putStrLn . ("TMVar " ++) $ show i
 
-  where randomSleep = randomRIO (1, 5) >>= sleep
-        update (s, _) = (s ++ "a", (length s) + 1)
+  where sleepOne = sleep 1
+        randomSleep = randomRIO (1, 5) >>= sleep
+        update (s, _) = let i = (read s) :: Int; x = fromIntegral(i * 1000); y = fromIntegral(i * 2000) in (show (i + 1), calc x y)
+        -- calc :: (Num a, Floating a) => a -> a -> a
+        calc x y = sqrt(x*x+y*y)+3*cos(sqrt(x*x+y*y))+5
