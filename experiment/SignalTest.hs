d212692b3af4aef8fb130fc998804f9210eea995
Zach Moazeni <zach@collectiveidea.com>
2012-04-18 11:19:35 -0400

    Testing multiple handlers

--- experiment/SignalTest.hs
+++ experiment/SignalTest.hs
@@ -2,6 +2,9 @@ import System.Posix.Signals
 import Control.Concurrent
 import Control.Concurrent.MVar
 
+oldHandler :: IO ()
+oldHandler = putStrLn "In old handler"
+
 handler :: MVar Bool -> IO ()
 handler keepRunning = do putStrLn "In handler"
                          swapMVar keepRunning False
@@ -17,6 +20,7 @@ keepRepeating keepRunning = readMVar keepRunning >>= doRepeat
 
 main :: IO ()
 main = do keepRunning <- newMVar True
+          installHandler sigINT (Catch oldHandler) Nothing
           installHandler sigINT (Catch (handler keepRunning)) Nothing
           keepRepeating keepRunning
   
